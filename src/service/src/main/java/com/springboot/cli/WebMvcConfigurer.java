11c85d8b61458da3a0136bd3962bbdfada52de29
xiaoshuai <xshuai1992@163.com>
2018-12-05 23:17:46 +0800

    按照 https://blog.csdn.net/sinat_27639721/article/details/78229040 完成前后端合并

--- src/service/src/main/java/com/springboot/cli/WebMvcConfigurer.java
+++ src/service/src/main/java/com/springboot/cli/WebMvcConfigurer.java
@@ -1,11 +1,11 @@
-package com.springboot.cli.page;
+package com.springboot.cli;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class SpringWebMvcConfig extends WebMvcConfigurerAdapter {
+public class WebMvcConfigurer extends WebMvcConfigurerAdapter {
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("classpath:/static/");
